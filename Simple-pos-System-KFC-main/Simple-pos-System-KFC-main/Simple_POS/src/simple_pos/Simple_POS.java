2a8293ea243d81257899002a131d238e23b4eaaa
DEVGRU <103409913+WhiskeyTangoFoxtro@users.noreply.github.com>
2024-11-06 22:29:10 +0530

    Add files via upload

---
+++ Simple-pos-System-KFC-main/Simple-pos-System-KFC-main/Simple_POS/src/simple_pos/Simple_POS.java
@@ -0,0 +1,18 @@
+
+
+package simple_pos;
+
+/**
+ *
+ *
+ */
+public class Simple_POS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
